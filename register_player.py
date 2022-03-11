9bf34ddc5f5d124c8d13e79f66612e625d1e5c75
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-03-11 21:00:39 +0000

    connected to database using psycopyg

--- register_player.py
+++ register_player.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from tkinter import EXCEPTION
 from fastapi import FastAPI
 from pydantic import BaseModel
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 # since we will need our data to be persistent, we will use Postgres as our database
 
@@ -12,3 +14,12 @@ class Player(BaseModel):
   last_name: str
   date_of_birth: datetime
   nationality: str
+
+try: 
+  conn = psycopg2.connect(database='tennis-club', cursor_factory=RealDictCursor) #real dict cursor gives you the column names too of the database
+  cursor = conn.cursor()
+  print('Database Connection was successful')
+
+except Exception as error:
+  print('Database connection FAILED')
+  
