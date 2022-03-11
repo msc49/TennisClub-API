cfa050da41dd49a955690809a905c09b187f8e5e
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-03-11 20:41:18 +0000

    added registered date

--- db/migrations/01_create_tennis_players.sql
+++ db/migrations/01_create_tennis_players.sql
@@ -10,8 +10,9 @@ CREATE TABLE players (player serial PRIMARY KEY,
   date_of_birth DATE NOT NULL,
   check(date_of_birth < (current_date - interval '16' year)),
   nationality VARCHAR (50) NOT NULL,
+  registered_date timestamp DEFAULT current_timestamp NOT NULL,
   player_score INT DEFAULT 1200
 
 );
 
--- \i ./db/migrations/01_create_tennis_players.sql;
+-- \i ./db/migrations/01_create_tennis_players.sql
