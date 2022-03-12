c8fa72567f4bc08950818f72665379febe5d60a5
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-03-12 16:29:52 +0000

    created playermodel

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
+
+class Player(BaseModel): #serializer
+  id: int
+  first_name: str
+  last_name: str
+  date_of_birth: datetime
+  nationality: str
+  player_score: int
+
+
 
 
 @app.get("/")
