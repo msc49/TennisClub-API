e2aa2063f138356e479de9de5aa5b38b653cc053
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-03-13 21:47:22 +0000

    matches table created

--- db/01_add_constraint.sql
+++ db/01_add_constraint.sql
@@ -1,18 +1,24 @@
-\c tennis_club
+-- \c tennis_club
 
-CREATE TABLE players (player serial PRIMARY KEY,
-  first_name VARCHAR (50) NOT NULL,
-  last_name VARCHAR (50) NOT NULL,
-  UNIQUE(first_name, last_name),
-  age INT NOT NULL,
-  check(age >= 16),
-  nationality VARCHAR (50) NOT NULL,
-  player_score INT DEFAULT 1200,
-  games_played INT DEFAULT 0
+-- CREATE TABLE players (player serial PRIMARY KEY,
+--   first_name VARCHAR (50) NOT NULL,
+--   last_name VARCHAR (50) NOT NULL,
+--   UNIQUE(first_name, last_name), -- checks if the combination of first name and last name are unique
+--   age INT NOT NULL,
+--   check(age >= 16), -- checks if the player is old enough
+--   nationality VARCHAR (50) NOT NULL,
+--   player_score INT DEFAULT 1200,
+--   games_played INT DEFAULT 0
   
 
-);
+-- );
 
-ALTER TABLE players ADD rank_name VARCHAR(50) NULL;
+-- ALTER TABLE players ADD rank_name VARCHAR(50) NULL;
+
+CREATE TABLE matches(match serial PRIMARY KEY,
+winner_id INT REFERENCES players(player),
+loser_id INT  REFERENCES players(player)
+
+);
 
 
